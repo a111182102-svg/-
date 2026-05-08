2df26134b5bef6842be572f9c8198fb54d6d303b
a111182102-svg <a111182102@nkust.edu.tw>
2026-05-08 17:03:40 +0000

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -9,9 +9,9 @@ export const PERSONAL_INFO = {
   zodiac: '牡羊座',
   socials: [
     { name: 'LinkedIn', url: '#', icon: Linkedin },
-    { name: 'Instagram', url: '#', icon: Instagram },
-    { name: 'YouTube', url: '#', icon: Youtube },
-    { name: 'Facebook', url: '#', icon: Facebook },
+    { name: 'Instagram', url: 'https://www.instagram.com/wumin.__.0327/?hl=zh-tw', icon: Instagram },
+    { name: 'YouTube', url: 'https://www.youtube.com/@-kd8ch', icon: Youtube },
+    { name: 'Facebook', url: 'https://www.facebook.com/wu.minmin.37167/', icon: Facebook },
     { name: 'X', url: '#', icon: Twitter },
   ]
 };
